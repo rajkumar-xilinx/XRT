e16d138b36bf8b6d828054915f89c004c84ab8f7
Rajkumar Rampelli <rajkumar@xilinx.com>
2022-03-24 07:15:40 +0530

    fix review comments
    Signed-off-by: Rajkumar Rampelli <rajkumar@xilinx.com>

--- src/runtime_src/core/pcie/driver/linux/xocl/userpf/xocl_drv.c
+++ src/runtime_src/core/pcie/driver/linux/xocl/userpf/xocl_drv.c
@@ -1080,13 +1080,26 @@ int xocl_hwmon_sdm_init(struct xocl_dev *xdev)
 	if (ret && ret != -EEXIST)
 		return ret;
 
-	(void) xocl_hwmon_sdm_init_sysfs(xdev, XCL_SDR_BDINFO);
-	(void) xocl_hwmon_sdm_init_sysfs(xdev, XCL_SDR_TEMP);
-	(void) xocl_hwmon_sdm_init_sysfs(xdev, XCL_SDR_CURRENT);
-	(void) xocl_hwmon_sdm_init_sysfs(xdev, XCL_SDR_POWER);
-	(void) xocl_hwmon_sdm_init_sysfs(xdev, XCL_SDR_VOLTAGE);
+	ret = xocl_hwmon_sdm_init_sysfs(xdev, XCL_SDR_BDINFO);
+	if (ret)
+		goto done;
 
-	return 0;
+	ret = xocl_hwmon_sdm_init_sysfs(xdev, XCL_SDR_TEMP);
+	if (ret)
+		goto done;
+
+	ret = xocl_hwmon_sdm_init_sysfs(xdev, XCL_SDR_CURRENT);
+	if (ret)
+		goto done;
+
+	ret = xocl_hwmon_sdm_init_sysfs(xdev, XCL_SDR_POWER);
+	if (ret)
+		goto done;
+
+	ret = xocl_hwmon_sdm_init_sysfs(xdev, XCL_SDR_VOLTAGE);
+
+done:
+	return ret;
 }
 
 /*
